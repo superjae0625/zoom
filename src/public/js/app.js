0f18f022c3e38885036d0d53632d3ef757e89975
jaehyungpark <superjae0625@gmail.com>
2021-10-06 01:19:22 -0600

    Sender

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -82,6 +82,13 @@ function handleCameraClick () {
 
 async function handleCameraChange () {
     await getMedia( camerasSelect.value );
+    if ( myPeerConnection ) {
+        const videoTrack = myStream.getVideoTracks()[ 0 ];
+        const videoSender = myPeerConnection
+            .getSenders()
+            .find( ( sender ) => sender.track.kind === "video" );
+        videoSender.replaceTrack( videoTrack );
+    }
 }
 
 muteBtn.addEventListener( "click", handleMuteClick );
@@ -121,23 +128,43 @@ socket.on( "welcome", async () => {
 } );
 
 socket.on( "offer", async ( offer ) => {
+    console.log( "received the offer" );
     myPeerConnection.setRemoteDescription( offer );
     const answer = await myPeerConnection.createAnswer();
     myPeerConnection.setLocalDescription( answer );
     socket.emit( "answer", answer, roomName );
+    console.log( "sent the answer" );
 } );
 
 socket.on( "answer", ( answer ) => {
+    console.log( "received the answer" );
     myPeerConnection.setRemoteDescription( answer );
+} );
+
+socket.on( "ice", ( ice ) => {
+    console.log( "received candidate" );
+    myPeerConnection.addIceCandidate( ice );
 } );
 
 // RTC Code
 
 function makeConnection () {
     myPeerConnection = new RTCPeerConnection();
+    myPeerConnection.addEventListener( "icecandidate", handleIce );
+    myPeerConnection.addEventListener( "addstream", handleAddStream );
     myStream
         .getTracks()
         .forEach( ( track ) => myPeerConnection.addTrack( track, myStream ) );
+}
+
+function handleIce ( data ) {
+    console.log( "sent candidate" );
+    socket.emit( "ice", data.candidate, roomName );
+}
+
+function handleAddStream ( data ) {
+    const peerFace = document.getElementById( "peerFace" );
+    peerFace.srcObject = data.stream;
 }
 
 
